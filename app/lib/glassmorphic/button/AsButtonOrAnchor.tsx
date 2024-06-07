b16c0837528e1bd1248beb6c45d3608df0cacd84
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-06-07 14:59:46 +0800

    Use `<Link>`

--- app/lib/glassmorphic/button/AsButtonOrAnchor.tsx
+++ app/lib/glassmorphic/button/AsButtonOrAnchor.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import { type ComponentPropsWithoutRef } from 'react';
 
 export const AsButtonOrAnchor = (
 	props:
 		| ComponentPropsWithoutRef<'button'>
 		| (ComponentPropsWithoutRef<'a'> & { href: string }),
-) => ('href' in props ? <a {...props} /> : <button {...props} />);
+) =>
+	'href' in props ? (
+		<Link {...(props as ComponentPropsWithoutRef<typeof Link>)} />
+	) : (
+		<button {...props} />
+	);
