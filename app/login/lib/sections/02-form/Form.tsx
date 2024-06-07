eaf5ee039cb596d8f9e80080b432be362c62327d
2021107221 <2021107221@student.uitm.edu.my>
2024-06-07 15:18:26 +0800

    Login page

--- app/login/lib/sections/02-form/Form.tsx
+++ app/login/lib/sections/02-form/Form.tsx
@@ -19,7 +19,7 @@ const WelcomeBack = () => {
 				<label className='inline-flex items-center'>
 					<input
 						type='checkbox'
-						className='h-3.5 w-3.5 appearance-none rounded-full p-3 backdrop-blur checked:bg-black/20'
+						className='h-4 w-4 appearance-none rounded-full backdrop-blur checked:bg-black/20'
 					/>
 					<span className='ml-2 text-base leading-9'>
 						{' '}
