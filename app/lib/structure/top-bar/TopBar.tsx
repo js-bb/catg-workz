f81f14bffffd50ce6e9bee41452044e0c9838520
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-06-07 12:09:44 +0800

    Don't apply props to image directly

--- app/lib/structure/top-bar/TopBar.tsx
+++ app/lib/structure/top-bar/TopBar.tsx
@@ -39,15 +39,16 @@ export const TopBar = ({ className }: Props.WithClassName<{}>) => {
 				<div className='h3'>Hello, Amanda!</div>
 				<Panel
 					as={(props) => (
-						<Image
-							src={amanda}
-							alt='Profile picture'
-							{...props}
-							className={clsx(
-								props.className,
-								'h-10 w-10 rounded-full border border-white/30 object-cover',
-							)}
-						/>
+						<div {...props}>
+							<Image
+								src={amanda}
+								alt='Profile picture'
+								className={clsx(
+									props.className,
+									'h-10 w-10 rounded-full border border-white/30 object-cover',
+								)}
+							/>
+						</div>
 					)}
 				/>
 			</div>
