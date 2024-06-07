1986b1381169b9efd8a23188a86fd3aae01ce5db
2021107221 <2021107221@student.uitm.edu.my>
2024-06-07 15:00:47 +0800

    Login page done

--- app/login/lib/sections/02-form/Form.tsx
+++ app/login/lib/sections/02-form/Form.tsx
@@ -1,27 +1,42 @@
 const WelcomeBack = () => {
 	return (
 		<>
-			<div className='flex h-72 w-full flex-col justify-start px-[60.5px]'>
-				<h1 className='text-left text-lg font-medium leading-9'>
+			<div className='mb-14 flex w-full flex-col justify-start px-[60.5px]'>
+				<h1 className='text-left text-md font-medium leading-9'>
 					User ID
 				</h1>
 				<input
-					className='mb-5 h-10 w-full rounded-full bg-black/20 p-3 pl-7 text-lg text-white
-						backdrop-blur'
+					className='mb-5 h-10 w-full rounded-full bg-black/20 p-3 text-md backdrop-blur'
 					placeholder='Type Here'
 				/>
-				<h1 className='text-left text-lg font-medium leading-9'>
+				<h1 className='text-left text-md font-medium leading-9'>
 					Password
 				</h1>
 				<input
-					className='mb-3 h-10 w-full rounded-full bg-black/20 p-3 pl-7 text-lg text-white
-						backdrop-blur'
+					className='mb-3 h-10 w-full rounded-full bg-black/20 p-3 text-md backdrop-blur'
 					placeholder='Type Here'
 				/>
-				<input
-					type='radio'
-					className='h-4 w-4 rounded-full bg-black/20 p-3 pl-7 backdrop-blur'
-				/>
+				<label className='inline-flex items-center'>
+					<input
+						type='checkbox'
+						className='h-[17px] w-[17px] appearance-none rounded-full p-3 backdrop-blur
+							checked:bg-black/20'
+					/>
+					<span className='ml-2 text-xs leading-9'>
+						{' '}
+						Remember me on this device for 30 days
+					</span>
+				</label>
+				<div className='flex justify-center'>
+					<button
+						className='mt-4 flex h-8 w-28 items-center justify-center gap-[0.125rem] rounded-full
+							bg-[#0181FC] px-3 py-1.5'
+					>
+						<span className='inline-block text-xs font-semibold'>
+							Login
+						</span>
+					</button>
+				</div>
 			</div>
 		</>
 	);
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,10 +5,10 @@ import Form from './lib/sections/02-form/Form';
 
 const Login = () => {
 	return (
-		<div className='flex h-full w-full flex-col items-center justify-center'>
+		<div className='flex flex-col items-center justify-center'>
 			<AmbientBackground className='items-center justify-center' />
 			<Panel
-				className='mt-44 flex w-1/4 w-[472px] flex-grow basis-[auto] flex-col flex-wrap
+				className='mb-48 mt-44 flex w-[472px] flex-grow basis-[auto] flex-col flex-wrap
 					items-center justify-center'
 			>
 				<WelcomeBack />
